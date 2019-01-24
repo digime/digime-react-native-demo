75afecf89e56c7321964ae92e3c754d3f554be3d
Oli <oliver@digi.me>
2019-01-24 17:27:33 +0000

    Update default contract to use new Medical objects

--- src/constants/Constants.ts
+++ src/constants/Constants.ts
@@ -1,7 +1,7 @@
 export class Constants {
     public static readonly APPLICATION_NAME: string = "[YOUR APPLICATION_NAME]";
     public static readonly APPLICATION_ID: string = "[YOUR APPLICATION_ID]";
-    public static readonly CONTRACT_ID: string = "gzqYsbQ1V1XROWjmqiFLcH2AF1jvcKcg";
+    public static readonly CONTRACT_ID: string = "fJI8P5Z4cIhP3HawlXVvxWBrbyj5QkTF";
     public static readonly P12_PASSPHRASE: string = "[YOUR P12_PASSWORD]";
     public static readonly P12_FILENAME: string = "CA_RSA_PRIVATE_KEY";
     public static readonly P12_EXTENSION: string = ".p12";
